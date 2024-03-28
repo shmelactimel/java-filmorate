e7b944c62cee7d6aef1b1957a8ec786a7720c80a
shmelactimel <2002erra@gmail.com>
2024-03-28 17:19:24 +0300

    ТЗ 11, Итерация 1 v.4

--- src/test/java/ru/yandex/practicum/filmorate/storage/impl/dao/GenreDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/impl/dao/GenreDbStorageTest.java
@@ -1,4 +1,4 @@
-package ru.yandex.practicum.storage.impl.dao;
+package ru.yandex.practicum.filmorate.storage.impl.dao;
 
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.BeforeEach;
